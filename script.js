f22a0f054ff2a36c80e8e84978c6af910960b527
Eliana <133684094+EllieVG@users.noreply.github.com>
2024-10-10 16:56:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('APISyPago.json')
+        .then(response => response.json())
+        .then(data => {
+            const dataDisplay = document.getElementById("dataDisplay");
+
+            // Create HTML elements to display the JSON data
+            const nameElement = document.createElement("p");
+            nameElement.textContent = "Link: " + data.pay_link;
+        
+            dataDisplay.appendChild(nameElement);
+ 
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+});
