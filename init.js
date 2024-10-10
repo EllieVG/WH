c7b5455934e53f287d71d1cf88a8ba4bab43de02
Eliana <133684094+EllieVG@users.noreply.github.com>
2024-10-10 11:34:48 -0400

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,15 @@
+$(funtion (){
+  loadScript()
+})
 
+funtion loadScript (url, callback) {
+
+  var head = document.head
+  var script = document.createElement("script")
+  script.type = "text/javascript"
+  script.scr = url
+  script.onreadystatechange = callback
+  script.onload = callback
+  head.appendChild(script)
+  
+}
